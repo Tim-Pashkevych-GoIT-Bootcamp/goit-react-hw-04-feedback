23cc3b34b2c070e8d85e647699661d8f2960f754
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-01-15 15:57:31 +0200

    Refactoring using React-hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,57 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import GlobalStyle from 'styles/GlobalStyle';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-export default class App extends Component {
-  state = { good: 0, neutral: 0, bad: 0 };
+export const App = () => {
+  const [points, setPoints] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  countTotalFeedback = () =>
-    Object.values(this.state).reduce((total, count) => (total += count), 0);
+  const countTotalFeedback = () =>
+    Object.values(points).reduce((total, count) => (total += count), 0);
 
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    return total ? ((this.state.good / total) * 100).toFixed() : 0;
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total ? ((points.good / total) * 100).toFixed() : 0;
   };
 
-  onLeaveFeedback = feedbackType => {
-    this.setState(prevState => ({
-      [feedbackType]: prevState[feedbackType] + 1,
+  const onLeaveFeedback = feedbackType => {
+    setPoints(prev => ({
+      ...prev,
+      [feedbackType]: prev[feedbackType] + 1,
     }));
   };
 
-  getStatisticsData = () => {
-    const total = this.countTotalFeedback();
-    const percentage = this.countPositiveFeedbackPercentage();
+  const getStatisticsData = () => {
+    const total = countTotalFeedback();
+    const percentage = countPositiveFeedbackPercentage();
     const positivePercentage = `${percentage}%`;
 
     return {
-      ...this.state,
+      ...points,
       total,
       positivePercentage,
     };
   };
 
-  render() {
-    return (
-      <>
-        <GlobalStyle />
+  return (
+    <>
+      <GlobalStyle />
 
-        <Section title="Please leave Feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedback() ? (
-            <Statistics data={this.getStatisticsData()} />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+      <Section title="Please leave Feedback">
+        <FeedbackOptions
+          options={Object.keys(points)}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() ? (
+          <Statistics data={getStatisticsData()} />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </>
+  );
+};
